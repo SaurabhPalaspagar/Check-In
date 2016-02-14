388a0d4caa6c369f8b6bd25a129d68f99e68984d
SaurabhPalaspagar <saurabh.palaspagar@outlook.com>
2016-02-14 10:14:38 -0500

    2Commit

--- app/src/main/java/com/example/saurabh/checkin/MainActivity.java
+++ app/src/main/java/com/example/saurabh/checkin/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.getpebble.android.kit.PebbleKit;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
@@ -25,13 +27,61 @@ import com.google.zxing.integration.android.IntentResult;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.encoder.QRCode;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Button scanBtn;
     private TextView formatTxt, contentTxt;
+    public  String pebbleQRCode;
+
+    //PebbleKit (Test of sending a notification to pebble watch)
+    @Override
+    protected void onResume() {
+        super.onResume();
+        boolean isConnected = PebbleKit.isWatchConnected(this);
+        Toast.makeText(this, "Pebble " + (isConnected ? "is" : "is not") + " connected!", Toast.LENGTH_LONG).show();
+        final Intent i = new Intent("com.getpebble.action.SEND_NOTIFICATION");
+
+        final Map data = new HashMap();
+        data.put("title", "Test Message");
+        data.put("body", 1234);
+        final JSONObject jsonData = new JSONObject(data);
+        final String notificationData = new JSONArray().put(jsonData).toString();
+
+        i.putExtra("messageType", "PEBBLE_ALERT");
+        i.putExtra("sender", "PebbleKit Android");
+        i.putExtra("notificationData", notificationData);
+        sendBroadcast(i);
+    }
+
+    //Send QR code to Pebble
+    public void pebble(View view){
+
+        boolean isConnected= PebbleKit.isWatchConnected(this);
+        Toast.makeText(this, "Pebble " + (isConnected ? "is" : "is not") + " connected!", Toast.LENGTH_LONG).show();
+        final Intent i = new Intent("com.getpebble.action.SEND_NOTIFICATION");
+        final Map data = new HashMap();
+        data.put("title", "Test Message");
+        data.put("body", 1234);
+        final JSONObject jsonData = new JSONObject(data);
+        final String notificationData = new JSONArray().put(jsonData).toString();
+
+        i.putExtra("messageType", "PEBBLE_ALERT");
+        i.putExtra("sender", "PebbleKit Android");
+        i.putExtra("notificationData", notificationData);
+        sendBroadcast(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +90,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+      //  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+      //  fab.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+      //      public void onClick(View view) {
+        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+          //              .setAction("Action", null).show();
+            //}
+        //});
         scanBtn = (Button)findViewById(R.id.scan_button);
         formatTxt = (TextView)findViewById(R.id.scan_format);
         contentTxt = (TextView)findViewById(R.id.scan_content);
@@ -68,7 +118,6 @@ public class MainActivity extends AppCompatActivity {
     //Generate QR code
     public  void generateQRCode(View view){
 
-
         QRCodeWriter writer=new QRCodeWriter();
         try{
             qrString=(EditText)this.findViewById(R.id.stringForQR);
@@ -83,15 +132,26 @@ public class MainActivity extends AppCompatActivity {
                     bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
                 }
             }
-            ((ImageView) findViewById(R.id.img_result_qr)).setImageBitmap(bmp);
-        }
-            catch (WriterException e) {
+            ((ImageView) findViewById(R.id.img_result_qr)).setImageBitmap(bmp);  //Set QR code image in Application
 
-            }
+
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.PNG, 40, bytes);
+
+            //you can create a new file name "test.png" in sdcard folder.
+            File f = new File(Environment.getExternalStorageDirectory()+ File.separator + "test.png");
+            f.createNewFile();
+            //write the bytes in file
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            // remember close de FileOutput
+            fo.close();
+        }
+            catch (IOException e){}
+            catch(WriterException e){}
         }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-
         if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
                 String contents = intent.getStringExtra("SCAN_RESULT"); // This will contain your scan result
@@ -105,6 +165,7 @@ public class MainActivity extends AppCompatActivity {
             String scanFormat = scanningResult.getFormatName();
             formatTxt.setText("FORMAT: " + scanFormat);
             contentTxt.setText("CONTENT: " + scanContent);
+            scanContent=scanContent;
         }
         else{
             Toast toast = Toast.makeText(getApplicationContext(),
